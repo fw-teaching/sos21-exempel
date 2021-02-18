81866a51ad70c63f17b61fdbd42d698f81a13cef
Fredrik Welander <fredrik.welander@subsite.fi>
2021-02-18 09:39:31 +0200

    Tar bort kommentar

--- app/src/main/java/fi/arcada/android/MainActivity.java
+++ app/src/main/java/fi/arcada/android/MainActivity.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    // kommentar 3
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
