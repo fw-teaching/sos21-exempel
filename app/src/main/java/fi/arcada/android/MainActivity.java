11b919e4162187f1c19f28d116f41bd2e38cc9a7
Fredrik Welander <fredrik.welander@subsite.fi>
2021-02-18 10:44:46 +0200

    Lägger till EditText och TextView och knapp samt handler för dessa.

--- app/src/main/java/fi/arcada/android/MainActivity.java
+++ app/src/main/java/fi/arcada/android/MainActivity.java
@@ -4,11 +4,34 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
 public class MainActivity extends AppCompatActivity {
+
+    // https://medium.com/@doyouseeitmyway/initialize-views-inside-of-oncreate-d72237b6f870
+    EditText editTextName;
+    EditText editTextNumberSigned;
+    TextView textViewOut;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editTextName = findViewById(R.id.editTextName);
+        editTextNumberSigned = findViewById(R.id.editTextNumberSigned);
+        textViewOut = findViewById(R.id.textViewOut);
+
+    }
+
+    public void calculate(View view) {
+
+        String labelText = editTextName.getText().toString();
+        double numberToCalculate = Double.parseDouble(editTextNumberSigned.getText().toString());
+
+        textViewOut.setText(String.format("label är %s, siffran är %.2f", labelText, numberToCalculate));
+
     }
 }
