8056adced1e685104fd6fedd227a1448eb1d36f9
Fredrik Welander <fredrik.welander@subsite.fi>
2021-03-12 10:25:19 +0200

    Moving Average, better version!

--- app/src/main/java/fi/arcada/android/MainActivity.java
+++ app/src/main/java/fi/arcada/android/MainActivity.java
@@ -31,28 +31,45 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    /** Att räkna glidande medelvärde i kod
+     *
+     */
     public void movingAvg() {
-        // För exemplets skull deklarerar vi nu här
+        // För exemplets skull deklarerar vi allt inom den här metoden, vi behöver inte
+        // komma åt textViewMA utanför metoden, och vill hålla resten av koden städad
         TextView textViewMA;
         textViewMA = findViewById(R.id.textViewMA);
         // vår datamängd  0   1   2   3
         int[] dataset = {10, 22, 29, 2, 20, 41, 10, 33, 12, 24};
         // arrayList för glidande medelvärde
-        ArrayList<Integer> ma = new ArrayList<Integer>();
+        ArrayList<Integer> ma = new ArrayList<>();
+        ArrayList<Integer> betterMa = new ArrayList<>();
+
         int window = 4; // fönsterstorlek
 
         for (int i = window-1; i < dataset.length; i++) {
             // Ett sätt att göra det, men hur funkar den här metoden med större fönster
             // eller om vi vill ha en app där vi kan ändra fönsterstorlek?
-            ma.add((dataset[i]
+            ma.add((dataset[i-0]
                     +dataset[i-1]
                     +dataset[i-2]
                     +dataset[i-3])/4);
         }
 
-        textViewMA.setText(String.format("%s\n%s",
+        // Mycket bättre sätt, nu kan vi enkelt använda olika fönsterstorlekar!
+        for (int i = window-1; i < dataset.length; i++) {
+            int sum = 0;
+            // Inre loop för fönstret
+            for (int j = 0; j < window; j++) {
+                sum += dataset[i-j];
+            }
+            betterMa.add(sum / window);
+        }
+
+        textViewMA.setText(String.format("%s\n%s\n%s",
                 Arrays.toString(dataset),
-                ma.toString()
+                ma.toString(),
+                betterMa.toString()
         ));
 
     }
