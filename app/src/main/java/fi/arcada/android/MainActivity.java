93d94ccfa94c659e766732bce9fbfd705901557a
Fredrik Welander <fredrik.welander@subsite.fi>
2021-03-15 16:51:47 +0200

    Comments

--- app/src/main/java/fi/arcada/android/MainActivity.java
+++ app/src/main/java/fi/arcada/android/MainActivity.java
@@ -37,12 +37,23 @@ public class MainActivity extends AppCompatActivity {
     public void buttonClick(View view) {
 
         try {
+
+            // Double.parseDouble kan ge en NumberFormatException-error och crasha appen
+            // Genom att sätt in den i ett try/catch-block kan vi i stället behandla
+            // errorn själv och förhindra en crash.
             myNum = Double.parseDouble(myNumber.getText().toString());
             textViewOut.setText(String.format("Du skrev: %.2f", myNum ));
 
+        // i catch-uttrycket anger vi vilket felmeddelande vi vill fånga,
+        // NumberFormatException i det här fallet
         } catch (NumberFormatException e) {
 
+            // Här skriver vi den kod som ska köras om
+            // vi fångar en error, i det här fallet vill vi bara skriva ett meddelande åt användaren
             textViewOut.setText("Du skrev inte ett tal!");
+
+            // Hela felmeddelandet finns i variabeln e som definieras ovan i (NumberFormatException e),
+            // den kan vi vid behov använda för att få alla detaljer om felet.
         }
 
 
