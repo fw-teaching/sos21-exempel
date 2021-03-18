de21216ee0704f0bed9f6cb3d6474ffb6609f4dd
Fredrik Welander <fredrik.welander@subsite.fi>
2021-03-18 12:06:24 +0200

    Add Datapoint offset and graph.removeAllSeries();

--- app/src/main/java/fi/arcada/android/MainActivity.java
+++ app/src/main/java/fi/arcada/android/MainActivity.java
@@ -31,6 +31,13 @@ public class MainActivity extends AppCompatActivity {
     ArrayList<Double> graphData = new ArrayList<>(
             Arrays.asList(10.0, 22.0, 29.0, 2.0, 20.0, 41.0, 10.0, 33.0, 12.0, 24.0)
     );
+    // En 3 element kortare exempeldatamängd
+    // (för att simulera en kortare datamängd som vi skulle få t.ex. när vi räknar glidande medelvärde...)
+    ArrayList<Double> graphDataShorter = new ArrayList<>(
+            Arrays.asList(10.0, 22.0, 29.0, 2.0, 20.0, 41.0, 10.0)
+    );
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -60,10 +67,7 @@ public class MainActivity extends AppCompatActivity {
         // Skapa en dataserie (alltså en linje för diagrammet)
         LineGraphSeries<DataPoint> series = new LineGraphSeries<>();
 
-        // Skapa en dataserie till
-        LineGraphSeries<DataPoint> otherSeries = new LineGraphSeries<>();
-        // Den här linjen får en annan färg!
-        otherSeries.setColor(Color.GREEN);
+
 
         // Vi loopar igenom vår datamängd och lägger varje datapunkt till serien
         for (int i = 0; i < dataset.size(); i++) {
@@ -71,19 +75,33 @@ public class MainActivity extends AppCompatActivity {
             series.appendData(new DataPoint(i, dataset.get(i)),true, dataset.size());
         }
 
-        // För exemplets skull loopar vi igenom samma dataset (men plockar y-värdena bakvänt)
-        // och lägger till dessa värden till vår andra dataerie
-        for (int i = 0; i < dataset.size(); i++) {
-            otherSeries.appendData(new DataPoint(i, dataset.get(dataset.size()-i-1)),true, dataset.size());
+
+        // Vi skapar en dataserie till för vår kortare datamängd
+        LineGraphSeries<DataPoint> otherSeries = new LineGraphSeries<>();
+        // Loopa vår kortare datamängd
+        otherSeries.setColor(Color.GREEN);
+
+        for (int i = 0; i < graphDataShorter.size(); i++) {
+
+            // Om vi vill att vår kortare datamänd ska gå ändra till X-axelns slut
+            // och i stället inte visa någonting i börejan (som t.ex. glidande medelvärde skulle bete sig)
+            // så måste vi "knuffa framåt" vår datamängd.
+            // I det här fallet väljer jag att räkna ut skillnaden och spara i "int offset",
+            int offset = dataset.size() - graphDataShorter.size(); // = 3 i det här fallet
+
+            // I new DataPoint(x, y) lägger vi nu till skillnaden (i+offset), så då kommer den här linjen att flyttas framåt
+            // Första punkten i serien skulle alltså då vara DataPoint(0+3, 2.0) om offset = 3
+            otherSeries.appendData(new DataPoint(i+offset, graphDataShorter.get(i)),true, dataset.size());
         }
+
+        // Töm grafen från eventuella tidigare linjer (viktigt om vi vill t.ex. "refresha" grafen med ny data!)
+        graph.removeAllSeries();
 
         // Slutligen lägger vi till dataserierna till vår graf!
         graph.addSeries(series);
         graph.addSeries(otherSeries);
 
     }
-
-
 
     /** Att räkna glidande medelvärde i kod
      *
@@ -105,8 +123,10 @@ public class MainActivity extends AppCompatActivity {
 
         int window = 4; // fönsterstorlek
 
+
         // Vi loopar igenom börjandes från det första värdet där alla tidigare
         // värden ryms med i samma dataset, dvs fjärde värdet (index 3) om vårt fönster är 4
+        /*
         for (int i = window-1; i < dataset.length; i++) {
             // Ett sätt att göra det, men hur funkar den här metoden med större fönster
             // eller om vi vill ha en app där vi kan ändra fönsterstorlek?
@@ -114,7 +134,7 @@ public class MainActivity extends AppCompatActivity {
                     +dataset[i-1]
                     +dataset[i-2]
                     +dataset[i-3])/4);
-        }
+        }*/
 
         // Mycket bättre sätt, nu kan vi enkelt använda olika fönsterstorlekar!
         for (int i = window-1; i < dataset.length; i++) {
